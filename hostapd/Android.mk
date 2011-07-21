f92e84c2d89a9b50e069197bf5d2465ad391fa21
Dmitry Shmidt <dimitrysh@google.com>
2011-07-21 15:19:46 -0700

    Android: Define ANDROID_BRCM_P2P_PATCH for Broadcom specific builds
    This is combination of part from
    
    Dmitry Shmidt <dimitrysh@google.com>
    Thu, 21 Jul 2011 15:19:46 -0700
    Change-Id: Iafec4bedbd33836d0a64e7ea054d8a46ef8ec204
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    and
    
    Jean-Baptiste Queru <jbq@google.com>
    Tue, 15 Nov 2011 18:41:36 -0800
    Enable broadcom-specific patch with proper flag
    Change-Id: I5a0f5a2d65963deff3cd62b5e7ec090619c75a2c

--- hostapd/Android.mk
+++ hostapd/Android.mk
@@ -24,6 +24,10 @@ L_CFLAGS += -DVERSION_STR_POSTFIX=\"-$(PLATFORM_VERSION)\"
 # Set Android log name
 L_CFLAGS += -DANDROID_LOG_NAME=\"hostapd\"
 
+ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
+L_CFLAGS += -DANDROID_BRCM_P2P_PATCH
+endif
+
 # To force sizeof(enum) = 4
 ifeq ($(TARGET_ARCH),arm)
 L_CFLAGS += -mabi=aapcs-linux
--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -31,6 +31,10 @@ ifdef CONFIG_NO_ROAMING
 L_CFLAGS += -DCONFIG_NO_ROAMING
 endif
 
+ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
+L_CFLAGS += -DANDROID_BRCM_P2P_PATCH
+endif
+
 # Use Android specific directory for control interface sockets
 L_CFLAGS += -DCONFIG_CTRL_IFACE_CLIENT_DIR=\"/data/misc/wifi/sockets\"
 L_CFLAGS += -DCONFIG_CTRL_IFACE_DIR=\"/data/system/wpa_supplicant\"
