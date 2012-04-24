1d958189c0f383a281637b1c2b73bb2cd8d22f80
Yoni Divinsky <yoni.divinsky@ti.com>
2012-04-24 15:22:59 +0300

    p2p: consider age for the p2p scan results
    Cfg80211 caches the scan results according the
    channel number. Due to the 15 sec aging this
    might cause the user mode to see more than one
    scan result with the same BSSID, e.g. - one
    scan result for the p2p-device and one for the
    p2p-GO (once it's enabled).
    
    Fix this by updating the device entry only if
    the new peer entry is newer than the one
    previously stored.
    
    Signed-off-by: Yoni Divinsky <yoni.divinsky@ti.com>
    Signed-off-by: Victor Goldenshtein <victorg@ti.com>
    Signed-off-by: Igal Chernobelsky <igalc@ti.com>

--- src/p2p/p2p.h
+++ src/p2p/p2p.h
@@ -1168,7 +1168,8 @@ void p2p_rx_action(struct p2p_data *p2p, const u8 *da, const u8 *sa,
  * start of a pending operation, e.g., to start a pending GO negotiation.
  */
 int p2p_scan_res_handler(struct p2p_data *p2p, const u8 *bssid, int freq,
-			 int level, const u8 *ies, size_t ies_len);
+			 unsigned int age, int level, const u8 *ies,
+			 size_t ies_len);
 
 /**
  * p2p_scan_res_handled - Indicate end of scan results
