f02659a5a011c81df97b1018fe597625b741f57c
Eyal Shapira <eyal@wizery.com>
2012-07-20 23:39:50 +0300

    Android.mk: Define ANDROID_P2P
    Changes within #ifdef ANDROID_P2P are BRCM additions
    which are non BRCM specific and there should be included.
    That's in contrast of ANDROID_BRCM_P2P_PATCH which mark
    parts that are BRCM implementation specific (like device_ap_sme)
    and therefore would be compiled in only when compiling with BRCM
    driver.
    
    Change-Id: I031f0908055756b6e85b64c5151c18b8a73b9841
    Signed-off-by: Eyal Shapira <eyal@wizery.com>

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -33,6 +33,8 @@ ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
 L_CFLAGS += -DANDROID_BRCM_P2P_PATCH
 endif
 
+L_CFLAGS += -DANDROID_P2P
+
 # Use Android specific directory for control interface sockets
 L_CFLAGS += -DCONFIG_CTRL_IFACE_CLIENT_DIR=\"/data/misc/wifi/sockets\"
 L_CFLAGS += -DCONFIG_CTRL_IFACE_DIR=\"/data/system/wpa_supplicant\"
