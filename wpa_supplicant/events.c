CyanogenMod/android_external_wpa_supplicant_8_ti

commit a523a2884fe83c111c8f3855bf36620c7d79b016

wpa_s: don't reschedule the sched scan on each scan complete event
wpa_supplicant doesn't distinguish between NL80211_CMD_SCHED_SCAN_RESULTS
and NL80211_CMD_NEW_SCAN_RESULTS, this causing unnecessary rescheduling
of the sched scan on each scan complete event.

Reschedule the sched scan (stop + start) only if the scan results are from
the sched scan.

Signed-off-by: Victor Goldenshtein <victorg@ti.com>

wpa_supplicant/events.c
@@ -1163,7 +1163,8 @@ static int _wpa_supplicant_event_scan_results(struct wpa_supplicant *wpa_s,
 				return 0;
 			}
 #endif /* CONFIG_P2P */
-			wpa_supplicant_req_sched_scan(wpa_s);
+			if (data->scan_info.is_sched_scan_res)
+				wpa_supplicant_req_sched_scan(wpa_s);
 			wpa_supplicant_req_new_scan(wpa_s, timeout_sec,
 						    timeout_usec);
 		}

src/drivers/driver.h
@@ -3405,6 +3405,7 @@ union wpa_event_data {
 	/**
 	 * struct scan_info - Optional data for EVENT_SCAN_RESULTS events
 	 * @aborted: Whether the scan was aborted
+	 * @is_sched_scan_res: Whether the scan results are from the sched scan
 	 * @freqs: Scanned frequencies in MHz (%NULL = all channels scanned)
 	 * @num_freqs: Number of entries in freqs array
 	 * @ssids: Scanned SSIDs (%NULL or zero-length SSID indicates wildcard
@@ -3413,6 +3414,7 @@ union wpa_event_data {
 	 */
 	struct scan_info {
 		int aborted;
+		int is_sched_scan_res;
 		const int *freqs;
 		size_t num_freqs;
 		struct wpa_driver_scan_ssid ssids[WPAS_MAX_SCAN_SSIDS];

src/drivers/driver_nl80211.c
@@ -1589,7 +1589,7 @@ static void mlme_event_remain_on_channel(struct wpa_driver_nl80211_data *drv,
 
 
 static void send_scan_event(struct wpa_driver_nl80211_data *drv, int aborted,
-			    struct nlattr *tb[])
+			    int is_sched_scan_res, struct nlattr *tb[])
 {
 	union wpa_event_data event;
 	struct nlattr *nl;
@@ -1633,6 +1633,7 @@ static void send_scan_event(struct wpa_driver_nl80211_data *drv, int aborted,
 		info->freqs = freqs;
 		info->num_freqs = num_freqs;
 	}
+	info->is_sched_scan_res = is_sched_scan_res;
 	wpa_supplicant_event(drv->ctx, EVENT_SCAN_RESULTS, &event);
 }
 
@@ -2131,12 +2132,12 @@ static void do_process_drv_event(struct wpa_driver_nl80211_data *drv,
 		drv->scan_complete_events = 1;
 		eloop_cancel_timeout(wpa_driver_nl80211_scan_timeout, drv,
 				     drv->ctx);
-		send_scan_event(drv, 0, tb);
+		send_scan_event(drv, 0, 0, tb);
 		break;
 	case NL80211_CMD_SCHED_SCAN_RESULTS:
 		wpa_printf(MSG_DEBUG,
 			   "nl80211: New sched scan results available");
-		send_scan_event(drv, 0, tb);
+		send_scan_event(drv, 0, 1, tb);
 		break;
 	case NL80211_CMD_SCAN_ABORTED:
 		wpa_printf(MSG_DEBUG, "nl80211: Scan aborted");
@@ -2146,7 +2147,7 @@ static void do_process_drv_event(struct wpa_driver_nl80211_data *drv,
 		 */
 		eloop_cancel_timeout(wpa_driver_nl80211_scan_timeout, drv,
 				     drv->ctx);
-		send_scan_event(drv, 1, tb);
+		send_scan_event(drv, 1, 0, tb);
 		break;
 	case NL80211_CMD_AUTHENTICATE:
 	case NL80211_CMD_ASSOCIATE:

commit 1cb767508ec7f760e81d5f1c832fc8263c570c99

Fix wpa_s crash and kickstart sched scan (INTERNAL-SQ)
data may be NULL and should be checked before dereferencing it.
This can happen when scan times out for example.
Another bug fixed is that sched scan wouldn't be kick started
from anywhere. Previously it was started following a normal scan
which didn't find any results. Fix it by start it if it's not running.

(These issues were introduced by commit
a523a28 wpa_s: don't reschedule the sched scan on each scan complete event
and the fix should be squashed into it)

Signed-off-by: Eyal Shapira <eyal@wizery.com>
Signed-off-by: Victor Goldenshtein <victorg@ti.com>

wpa_supplicant/events.c
@@ -1231,7 +1231,8 @@ static int _wpa_supplicant_event_scan_results(struct wpa_supplicant *wpa_s,
 				return 0;
 			}
 #endif /* CONFIG_P2P */
-			if (data->scan_info.is_sched_scan_res)
+			if ((data && data->scan_info.is_sched_scan_res) ||
+			    !wpa_s->sched_scanning)
 				wpa_supplicant_req_sched_scan(wpa_s);
 			wpa_supplicant_req_new_scan(wpa_s, timeout_sec,
 						    timeout_usec);