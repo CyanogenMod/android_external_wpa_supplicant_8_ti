5daa41748cec1bef77b34d29e7bfe79ed68c050a
Dmitry Shmidt <dimitrysh@google.com>
2013-08-16 15:29:47 -0700

    P2P: Add commands for p2p redirection
    Bug: 9056601
    
    Change-Id: I6d78433df656c24af74a6825624c68231a4bf87d
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -5984,6 +5984,10 @@ static char * wpas_global_ctrl_iface_redir_p2p(struct wpa_global *global,
 {
 #ifdef CONFIG_P2P
 	static const char * cmd[] = {
+#ifdef ANDROID_P2P
+		"LIST_NETWORKS",
+		"SAVE_CONFIG",
+#endif
 		"P2P_FIND",
 		"P2P_STOP_FIND",
 		"P2P_LISTEN",
@@ -5998,6 +6002,12 @@ static char * wpas_global_ctrl_iface_redir_p2p(struct wpa_global *global,
 		NULL
 	};
 	static const char * prefix[] = {
+#ifdef ANDROID_P2P
+		"DRIVER ",
+		"GET_NETWORK ",
+		"REMOVE_NETWORK ",
+		"SET ",
+#endif
 		"P2P_FIND ",
 		"P2P_CONNECT ",
 		"P2P_LISTEN ",
