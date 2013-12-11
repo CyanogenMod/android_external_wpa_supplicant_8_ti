2464e5d63604f1f2727666ccc36eff31368622c9
Arik Nemtsov <arik@wizery.com>
2013-12-11 13:56:06 +0200

    Android: driver_nl80211: implement NoA/p2p_ps/set_wps_ie stubs
    Most return -1, as if the functions were not implemented. This allows
    us to compile with ANDROID_P2P. set_wps_ie returns 0 to allow AP
    initialization to work.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Change-Id: I9a454782524270e3db5af45b0aadd051c6fa41a0

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -335,12 +335,31 @@ extern int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 					 size_t buf_len);
 #endif /* ANDROID */
 #ifdef ANDROID_P2P
-int wpa_driver_set_p2p_noa(void *priv, u8 count, int start, int duration);
-int wpa_driver_get_p2p_noa(void *priv, u8 *buf, size_t len);
-int wpa_driver_set_p2p_ps(void *priv, int legacy_ps, int opp_ps, int ctwindow);
+int wpa_driver_set_p2p_noa(void *priv, u8 count, int start, int duration)
+{
+	return -1;
+}
+
+
+int wpa_driver_get_p2p_noa(void *priv, u8 *buf, size_t len)
+{
+	return -1;
+}
+
+
+int wpa_driver_set_p2p_ps(void *priv, int legacy_ps, int opp_ps, int ctwindow)
+{
+	return -1;
+}
+
+
 int wpa_driver_set_ap_wps_p2p_ie(void *priv, const struct wpabuf *beacon,
 				 const struct wpabuf *proberesp,
-				 const struct wpabuf *assocresp);
+				 const struct wpabuf *assocresp)
+{
+	return 0;
+}
+
 #endif /* ANDROID_P2P */
 
 #ifdef HOSTAPD
